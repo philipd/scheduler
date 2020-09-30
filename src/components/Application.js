00a591daa4aedeecaf47d156159b8161812d2240
philipd <philip.dickau@gmail.com>
2020-09-30 14:02:12 -0600

    Display schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -20,9 +21,57 @@ const days = [
   },
 ];
 
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: interviewers[0]
+    }
+  },
+  {
+    id: 3,
+    time: "7pm",
+    interview: {
+      student: "Nubya Garcia",
+      interviewer: interviewers[2]
+    }
+  },
+  {
+    id: 4,
+    time: "10am",
+    interview: {
+      student: "Andrew Hill",
+      interviewer: interviewers[3]
+    }
+  },
+  {
+    id: 5,
+    time: "10pm",
+    interview: {
+      student: "Wayne Shorter",
+      interviewer: interviewers[4]
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
 
+  console.log(appointments);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -49,7 +98,11 @@ export default function Application(props) {
 
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment =>
+          <Appointment
+            key={appointment.id} {...appointment}
+          />)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,21 @@ import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
 
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
+
 export default function Appointment(props) {
   return <article className="appointment">
-    <Header time={ props.time }/>
-    { props.interview 
-      ? <Show 
-          student={ props.interview.student } 
-          interviewer={ props.interview.interviewer }/> 
-      : <Empty /> }
+    <Header time={props.time} />
+    {props.interview
+      ? <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer} />
+      : <Empty />}
   </article>;
 }
