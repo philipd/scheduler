a09d40ea7f93f229e475e684e3e293a2885cd8cb
philipd <philip.dickau@gmail.com>
2020-10-05 03:09:45 -0600

    Clean up

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -4,9 +4,6 @@ import axios from "axios";
 
 function reducer(state, action) {
   const stateCopy = deepcopy(state);
-  // console.log("hi from reducer");
-  console.log("reducer state", state);
-  console.log("reducer action", action);
 
   switch (action.type) {
     case "setDays":
@@ -21,15 +18,6 @@ function reducer(state, action) {
     case "bookInterview":
       stateCopy.appointments[action.value.id].interview = action.value.interview;
       return stateCopy;
-    // case "setDays":
-    //   stateCopy.days = action.value;
-    //   return stateCopy;
-    // case "setAppointments":
-    //   stateCopy.appointments = action.value;
-    //   return stateCopy;
-    // case "setInterviewers":
-    //   stateCopy.interviewers = action.value;
-    //   return stateCopy;
     case "load":
       stateCopy.days = action.value.days;
       stateCopy.appointments = action.value.appointments;
@@ -48,30 +36,9 @@ const initialState = {
 };
 
 export default function useApplicationData(initial) {
-  const [xstate, dispatch] = useReducer(reducer, initialState);
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const proxyHandler = {
-    get: function (target, prop, receiver) {
-      if (prop === "day") {
-        return xstate.day;
-      }
-      if (prop === "days") {
-        return xstate.days;
-      }
-      if (prop === "appointments") {
-        return xstate.appointments;
-      }
-      if (prop === "interviewers") {
-        return xstate.interviewers;
-      }
-      return Reflect.get(...arguments);
-    },
-  };
-
-  const stateProxy = new Proxy(state, proxyHandler);
-
-  const xsetDay = (xday) => dispatch({ type: "setDay", value: xday });
+  const setDay = (day) => dispatch({ type: "setDay", value: day });
 
   useEffect(() => {
     const daysPromise = axios.get("http://localhost:8001/api/days");
@@ -84,16 +51,6 @@ export default function useApplicationData(initial) {
 
     Promise.all([daysPromise, appointmentsPromise, interviewersPromise]).then(
       (responses) => {
-        // setState((prev) => ({
-        //   ...prev,
-        //   days: responses[0].data,
-        //   // appointments: responses[1].data,
-        //   interviewers: responses[2].data,
-        // }));
-
-        // dispatch({ type: "setAppointments", value: responses[1].data })
-        // dispatch({ type: "setInterviewers", value: responses[2].data })
-        // dispatch({ type: "setDays", value: responses[0].data })
         dispatch({
           type: "load",
           value: {
@@ -108,7 +65,6 @@ export default function useApplicationData(initial) {
 
   function loadSpots() {
     axios.get("http://localhost:8001/api/days").then((res) => {
-      // setState((prev) => ({ ...prev, days: res.data }));
       dispatch({ type:'setDays', value: res.data });
     });
   }
@@ -123,20 +79,9 @@ export default function useApplicationData(initial) {
   }
 
   function bookInterview(id, interview) {
-    // const appointment = {
-    //   ...state.appointments[id],
-    //   interview: { ...interview },
-    // };
-
-    // const appointments = {
-    //   ...state.appointments,
-    //   [id]: appointment,
-    // };
-
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, { interview })
       .then((res) => {
-        // setState({ ...state, appointments });
         console.log('response', interview);
         dispatch({ type: "bookInterview", value: { id, interview } });
         loadSpots();
@@ -144,8 +89,8 @@ export default function useApplicationData(initial) {
   }
 
   return {
-    state: stateProxy,
-    setDay: xsetDay,
+    state,
+    setDay,
     bookInterview,
     cancelInterview,
   };
