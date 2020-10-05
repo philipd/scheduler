0e891ca332a2d0ac55266c7cbd4a298e0b035719
philipd <philip.dickau@gmail.com>
2020-10-05 03:00:35 -0600

    Support deletion, no create or edit yet

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -4,13 +4,36 @@ import axios from "axios";
 
 function reducer(state, action) {
   const stateCopy = deepcopy(state);
+  // console.log("hi from reducer");
+  console.log("reducer state", state);
+  console.log("reducer action", action);
 
   switch (action.type) {
+    case "setDays":
+      stateCopy.days = action.value;
+      return stateCopy;
     case "setDay":
       stateCopy.day = action.value;
       return stateCopy;
     case "cancelInterview":
-      stateCopy.appointments[action.value] = null;
+      stateCopy.appointments[action.value].interview = null;
+      return stateCopy;
+    case "bookInterview":
+      stateCopy.appointments[action.value.id] = action.value.interview;
+      return stateCopy;
+    // case "setDays":
+    //   stateCopy.days = action.value;
+    //   return stateCopy;
+    // case "setAppointments":
+    //   stateCopy.appointments = action.value;
+    //   return stateCopy;
+    // case "setInterviewers":
+    //   stateCopy.interviewers = action.value;
+    //   return stateCopy;
+    case "load":
+      stateCopy.days = action.value.days;
+      stateCopy.appointments = action.value.appointments;
+      stateCopy.interviewers = action.value.interviewers;
       return stateCopy;
     default:
       throw new Error();
@@ -33,6 +56,15 @@ export default function useApplicationData(initial) {
       if (prop === "day") {
         return xstate.day;
       }
+      if (prop === "days") {
+        return xstate.days;
+      }
+      if (prop === "appointments") {
+        return xstate.appointments;
+      }
+      if (prop === "interviewers") {
+        return xstate.interviewers;
+      }
       return Reflect.get(...arguments);
     },
   };
@@ -52,20 +84,33 @@ export default function useApplicationData(initial) {
 
     Promise.all([daysPromise, appointmentsPromise, interviewersPromise]).then(
       (responses) => {
-        setState((prev) => ({
-          ...prev,
-          days: responses[0].data,
-          appointments: responses[1].data,
-          interviewers: responses[2].data,
-        }));
+        // setState((prev) => ({
+        //   ...prev,
+        //   days: responses[0].data,
+        //   // appointments: responses[1].data,
+        //   interviewers: responses[2].data,
+        // }));
+
+        // dispatch({ type: "setAppointments", value: responses[1].data })
+        // dispatch({ type: "setInterviewers", value: responses[2].data })
+        // dispatch({ type: "setDays", value: responses[0].data })
+        dispatch({
+          type: "load",
+          value: {
+            days: responses[0].data,
+            appointments: responses[1].data,
+            interviewers: responses[2].data,
+          },
+        });
       }
     );
   }, []);
 
   function loadSpots() {
-    axios
-      .get("http://localhost:8001/api/days")
-      .then((res) => setState((prev) => ({ ...prev, days: res.data })));
+    axios.get("http://localhost:8001/api/days").then((res) => {
+      // setState((prev) => ({ ...prev, days: res.data }));
+      dispatch({ type:'setDays', value: res.data });
+    });
   }
 
   function cancelInterview(id) {
@@ -78,20 +123,21 @@ export default function useApplicationData(initial) {
   }
 
   function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: { ...interview },
+    // };
 
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment,
+    // };
 
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, { interview })
       .then((res) => {
-        setState({ ...state, appointments });
+        // setState({ ...state, appointments });
+        dispatch({ type: "bookInterview", value: { id, interview } });
         loadSpots();
       });
   }
