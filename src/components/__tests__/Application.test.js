88de2a123158525afa2e2b006e8c533cbdd16d85
philipd <philip.dickau@gmail.com>
2020-10-06 23:57:05 -0600

    Complete activity

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -25,7 +25,6 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { debug, container } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
     fireEvent.click(getByAltText(appointment, "Add"));
@@ -34,7 +33,6 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
     expect(getByText(appointment, "Saving ...")).toBeInTheDocument();
-    
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
     const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
